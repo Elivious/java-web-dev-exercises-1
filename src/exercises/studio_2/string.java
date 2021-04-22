9d251c3dc37291d5464e6e8eecb20606242f392b
Marcus Admin R. Chinery <MarcusAdmin@todd.home.com>
2021-04-22 18:36:09 -0500

    modified:   src/exercises/studio_2/string.java 	modified:   src/exercises/studio_2/studio_2.java

--- src/exercises/studio_2/string.java
+++ src/exercises/studio_2/string.java
@@ -1,2 +1,23 @@
-package exercises.studio_2;public class string {
+package exercises.studio_2;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class string {
+    public static void main(String[] args) {
+        Scanner userString = new Scanner(System.in);
+        String userSubmission;
+        Boolean check;
+        do {
+            System.out.println("Please type in the string you would like to save.");
+            userSubmission = userString.nextLine();
+            if(userSubmission.matches(".*\\d.*")){
+                System.out.println("Please enter only alphabetical letters.");
+                check = false;
+            }else {
+                System.out.println("Sentence passed");
+                check = true;
+            }
+        }while(!check);
+    }
 }
--- src/exercises/studio_2/studio_2.java
+++ src/exercises/studio_2/studio_2.java
@@ -1,13 +1,32 @@
 package exercises.studio_2;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class studio_2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Path fileName = Path.of("C:/Users/MarcusAdmin/Desktop/string.txt");
         Scanner userString = new Scanner(System.in);
-        System.out.println("Please type in the string you would like to save.");
-        String userSubmission = userString.nextLine();
+        String userSubmission;
+        Boolean check;
+        do {
+            System.out.println("Please type in the string you would like to save.");
+            userSubmission = userString.nextLine();
+            if(userSubmission.matches(".*\\d.*")){
+                System.out.println("Please enter only alphabetical letters.");
+                check = false;
+            }else {
+                System.out.println("Sentence passed");
+                check = true;
+            }
+        }while(!check);
+
+        Files.writeString(fileName, userSubmission);
+        String actual = Files.readString(fileName);
+
        // String sample = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
         String search;
         Integer occurence = 0;
@@ -19,7 +38,7 @@ public class studio_2 {
                 System.out.println("Please enter only one letter");
             }
         }while (search.length() != 1);
-        char[] query = userSubmission.toLowerCase(Locale.ROOT).toCharArray();
+        char[] query = actual.toLowerCase(Locale.ROOT).toCharArray();
         char[] result = search.toLowerCase(Locale.ROOT).toCharArray();
         for(char i : query){
             if(i == result[0]){
